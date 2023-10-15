a60c3c97f088d915ef4e9947f4118183b5915f04
Peter Boakye <petboa123@gmail.com>
2023-10-15 22:43:00 +0000

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- Lists all bands with Glam rock as their main style, ranked by their longevity.
 -- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
 SELECT band_name, 
-       IFNULL(DATEDIFF(2022, 
+       IFNULL(DATEDIFF(2020, 
              STR_TO_DATE(SUBSTRING_INDEX(lifespan, '-', 1), '%Y'), 
              STR_TO_DATE(SUBSTRING_INDEX(lifespan, '-', -1), '%Y')), 0) AS lifespan
 FROM metal_bands
